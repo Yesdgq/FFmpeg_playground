dd65ee3a58c57099fc2bb0a6f99a2fdfb694e9de
董国庆 <yesdgq@yahoo.com>
2017-05-11 11:59:44 +0800

    initial commit

---
+++ FFmpeg_playground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFmpeg_playground
+//
+//  Created by yesdgq on 2017/5/11.
+//  Copyright © 2017年 yesdgq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFmpeg_playground/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FFmpeg_playground
+//
+//  Created by yesdgq on 2017/5/11.
+//  Copyright © 2017年 yesdgq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
